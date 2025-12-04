19dd2609f531c3455350f9771b0c7ad518a3aef9
António Sebastião Pedro <antoniosebastiaopedro19@gmail.com>
2025-12-04 07:56:26 +0100

    feat: coloque imagens fisicas ao invez de links

--- assets/js/data.js
+++ assets/js/data.js
@@ -5,7 +5,7 @@ const PRODUCTS = [
     price:350, 
     category:'Bebidas', 
     promo:true, 
-    image:'https://images.unsplash.com/photo-1581636625402-29b2a704ef13?w=600&h=400&fit=crop&crop=center'
+    image:'assets/images/photo-1581636625402-29b2a704ef13.jpeg'
   },
   {
     id:2, 
@@ -13,7 +13,7 @@ const PRODUCTS = [
     price:200, 
     category:'Bebidas', 
     promo:false, 
-    image:'https://images.unsplash.com/photo-1559839734-2b71ea197ec2?w=600&h=400&fit=crop&crop=center'
+    image:'assets/images/photo-1559839734-2b71ea197ec2.jpeg'
   },
   {
     id:3, 
@@ -21,7 +21,7 @@ const PRODUCTS = [
     price:850, 
     category:'Lanches', 
     promo:true, 
-    image:'https://images.unsplash.com/photo-1539252554453-80ab65ce3586?w=600&h=400&fit=crop&crop=center'
+    image:'assets/images/photo-1539252554453-80ab65ce3586.jpeg'
   },
   {
     id:4, 
@@ -29,7 +29,7 @@ const PRODUCTS = [
     price:450, 
     category:'Snacks', 
     promo:false, 
-    image:'https://images.unsplash.com/photo-1558961363-fa8fdf82db35?w=600&h=400&fit=crop&crop=center'
+    image:'assets/images/photo-1558961363-fa8fdf82db35.jpeg'
   },
   {
     id:5, 
@@ -37,7 +37,7 @@ const PRODUCTS = [
     price:300, 
     category:'Snacks', 
     promo:false, 
-    image:'https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?w=600&h=400&fit=crop&crop=center'
+    image:'assets/images/photo-1571019613454-1cb2f99b2d8b.jpeg'
   },
   {
     id:6, 
@@ -45,7 +45,7 @@ const PRODUCTS = [
     price:1250, 
     category:'Higiene', 
     promo:true, 
-    image:'https://images.unsplash.com/photo-1556228720-195a672e8a03?w=600&h=400&fit=crop&crop=center'
+    image:'assets/images/photo-1556228720-195a672e8a03.jpeg'
   },
   {
     id:7, 
@@ -53,7 +53,7 @@ const PRODUCTS = [
     price:650, 
     category:'Lanches', 
     promo:false, 
-    image:'https://images.unsplash.com/photo-1511381939415-e44015466834?w=600&h=400&fit=crop&crop=center'
+    image:'assets/images/photo-1511381939415-e44015466834.jpeg'
   },
   {
     id:8, 
@@ -61,7 +61,7 @@ const PRODUCTS = [
     price:1100, 
     category:'Outros', 
     promo:false, 
-    image:'https://images.unsplash.com/photo-1609592132160-c92b89b6b3b1?w=600&h=400&fit=crop&crop=center'
+    image:'assets/images/photo-1609592132160-c92b89b6b3b1.jpeg'
   },
   {
     id:9, 
@@ -69,7 +69,7 @@ const PRODUCTS = [
     price:550, 
     category:'Bebidas', 
     promo:true, 
-    image:'https://images.unsplash.com/photo-1570197788417-0e82375c9371?w=600&h=400&fit=crop&crop=center'
+    image:'assets/images/photo-1570197788417-0e82375c9371.jpeg'
   },
   {
     id:10, 
@@ -77,7 +77,7 @@ const PRODUCTS = [
     price:350, 
     category:'Snacks', 
     promo:false, 
-    image:'https://images.unsplash.com/photo-1566478989037-eec170784d0b?w=600&h=400&fit=crop&crop=center'
+    image:'assets/images/photo-1566478989037-eec170784d0b.jpeg'
   },
   {
     id:11, 
@@ -85,7 +85,7 @@ const PRODUCTS = [
     price:800, 
     category:'Higiene', 
     promo:false, 
-    image:'https://images.unsplash.com/photo-1556228453-efd6c1ff04f6?w=600&h=400&fit=crop&crop=center'
+    image:'assets/images/photo-1556228453-efd6c1ff04f6.jpeg'
   },
   {
     id:12, 
@@ -93,6 +93,6 @@ const PRODUCTS = [
     price:1500, 
     category:'Bebidas', 
     promo:true, 
-    image:'https://images.unsplash.com/photo-1497935586351-b67a49e012bf?w=600&h=400&fit=crop&crop=center'
+    image:'assets/images/photo-1497935586351-b67a49e012bf.jpeg'
   }
 ];
